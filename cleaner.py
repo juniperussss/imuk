af220a2020b0be517547bae87170d2868f048ec3
Kleeritter <steding@tutanota.com>
2022-09-12 14:00:42 +0200

    Improved automatic cleaning and creating of multiple timestep images

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,4 @@
-def cleaning_old_timefolders():
+def cleaning_old_today_folders():
     import os
     import shutil
     from datetime import datetime, timedelta
@@ -19,6 +19,27 @@ def cleaning_old_timefolders():
             print("removing", oldfolder)
             shutil.rmtree(oldfolder)
 
+def archiving():
+    import os
+    import shutil
+    import glob
+    print(os.getcwd())
+    filepath= os.getcwd()+"/database/output"
+    folderlevel=["300","700"]
+    dest= filepath +"/archiv/"
+    shutil.rmtree(dest)
+    os.mkdir(dest)
+    for i in range(len(folderlevel)):
+        src= filepath +"/"+folderlevel[i]
+        desti= dest +"/" +folderlevel[i]
+        shutil.copytree(src, desti, copy_function=shutil.copy)
+    print("archiving successful")
+    return
+
+
+
+
+
 def varnames(varnumber,varnames,varlevel):
     from datetime import datetime
     import os
@@ -35,3 +56,31 @@ def varnames(varnumber,varnames,varlevel):
 
     print(varalist)
     return varalist
+
+
+def cleaning_old_folders():
+    from datetime import datetime
+    import os
+    import glob
+    import shutil
+    today = datetime.now()
+    todaypath = os.getcwd() + "/database/input/icon/" + str(today.year) + "/" + str(today.month) + "/" + str(today.day)
+    todayyearpath=os.getcwd() + "/database/input/icon/" + str(today.year)
+    todaymonthpath=os.getcwd() + "/database/input/icon/" + str(today.year) + "/" + str(today.month)
+    allyearpaths= glob.glob(os.getcwd() +"/database/input/icon/"+"/*")
+    allmonthpaths=glob.glob(os.getcwd() + "/database/input/icon/" + str(today.year) + "/*")
+    alldaypaths=glob.glob(os.getcwd() + "/database/input/icon/" + str(today.year) + "/" + str(today.month) + "/*" )
+    print(allyearpaths)
+    for i in range(len(allyearpaths)):
+        if (allyearpaths[i] != todayyearpath):
+            print("removing old year folders")
+            shutil.rmtree(allyearpaths[i])
+    for i in range(len(allmonthpaths)):
+        if (allmonthpaths[i] != todaymonthpath):
+            print("removing old Month folders")
+            shutil.rmtree(allmonthpaths[i])
+    for i in range(len(alldaypaths)):
+        if (alldaypaths[i] != todaypath):
+            print("removing old Day folders")
+            shutil.rmtree(alldaypaths[i])
+    return
--- wind_300.py
+++ wind_300.py
@@ -14,7 +14,8 @@ import cleaner
 from datetime import date
 
 ### Cleaning and Setup
-cleaner.cleaning_old_timefolders() #Remove old Folders
+cleaner.cleaning_old_today_folders() #Remove old Folders for today
+cleaner.archiving()
 varnumber=2
 vars=["u","v"]
 varlevel=[300,300]
