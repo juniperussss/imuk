b0226939962ce940dc1fc29f562be82ff56cf773
Kleeritter <93885297+Kleeritter@users.noreply.github.com>
2022-11-29 12:56:24 +0100

    Update run_parameters.sh

--- run_parameters.sh
+++ run_parameters.sh
@@ -1,5 +1,5 @@
 #Wind
 /opt/conda/bin/python /workspaces/imuk/wind_300.py 1920 1080 /workspaces/imuk/database/output/300 /workspaces/imuk
-
+/home/alex/miniforge3/envs/imuk/bin/python /home/alex/imuk/wind_300.py 1920 1080 /home/alex/imuk/database/output/300 /workspaces/imuk
 #relhum
 #/opt/conda/bin/python /workspaces/imuk/gph_rh_700.py 1920 1080 /workspaces/imuk/database/output/700 /workspaces/imuk
