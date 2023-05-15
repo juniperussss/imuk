437d0adaae6f0affa26f7dbc1b8a5b639dacfabd
Kleeritter <kleeritter@duck.com>
2023-05-15 19:04:54 +0200

    Improve Fonts and Image Quality using different sampling method

--- ressources/tools/observations.py
+++ ressources/tools/observations.py
@@ -269,7 +269,7 @@ def metarrequest(dirorigin):
     datare=nclwwstring(datare)
     metar_groundlevel = datare.to_csv(dirorigin+'/ressources/Data/metar_groundlevel.csv', index = True)
 
-def main():
-    metarrequest("/home/alex/PycharmProjects/imuk")
+#def main():
+ #   metarrequest("/home/alex/PycharmProjects/imuk")
 
-main()
+#main()
