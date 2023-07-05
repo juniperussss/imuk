408741f697929ead7ea0bfc2fcb38303b7d10adf
juniperussss <94772818+juniperussss@users.noreply.github.com>
2023-07-05 19:09:35 +0200

    Update gph_temp_500.py

--- products/gph_maps/gph_temp_500.py
+++ products/gph_maps/gph_temp_500.py
@@ -322,7 +322,7 @@ def picture(vara,varb,number,resx,resy,dir_origin,filenames):
 
 
     hour,weekday,datetime_object,delta= imuktools.dates_for_subtitles(vara, number, filenames)
-    left_string_2   = '500 hPa: Geopotential(gdm), Temperatur (Grad  C)'  #model output info
+    left_string_2   = '500 hPa: Geopotential(gdm), Temperatur (Grad  ~F34~0~F~C)'  #model output info
     left_string   = 'ICON-Lauf: '  +weekday.capitalize() + " " +datetime_object.strftime('%d.%m.%Y %H')  +" UTC" +" (+"+delta+"h)"#model output info
     center_string = '' #center information bar
     # right_string_2 = 'Init: ' + str(initial_time)
