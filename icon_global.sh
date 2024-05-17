570b39a0ce262b52e241bb1da048a87421356c17
Alex Steding <kleeritter@duck.com>
2024-05-17 19:00:35 +0200

    h2

--- icon_global.sh
+++ icon_global.sh
@@ -22,6 +22,8 @@ ydim_2=420
 xdim_3=350
 ydim_3=210
 
+model=icon
+
 python /localdata/weathermaps/imuk/ressources/tools/request.py /localdata/weathermaps/imuk /localdata/weathermaps/imuk/database/input/icon
 echo "icon finished"
 
@@ -42,10 +44,10 @@ python /localdata/weathermaps/imuk/products/gph_maps/gph_temp_500.py $xdim_1 $yd
 echo "500  finished"
 python /localdata/weathermaps/imuk/products/gph_maps/gph_rh_700.py $xdim_1 $ydim_1 $path_output $path_input $start $end $stepsize
 echo "700  finished"
-python /localdata/weathermaps/imuk/products/gph_maps/bd_sw_meteosat.py $xdim_1 $ydim_1 $path_output $path_input $start $end $stepsize
+python /localdata/weathermaps/imuk/products/gph_maps/bd_sw_meteosat.py $xdim_1 $ydim_1 $path_output $path_input $start $end $stepsize $model
 
 python /localdata/weathermaps/imuk/ressources/tools/imageresizer.py $path_output_klein $path_output_4panel $path_output $xdim_3 $ydim_3 $xdim_2 $ydim_2
 echo "resizing  finished"
 
 ##Stationmaps
-python /localdata/weathermaps/imuk/products/stationmaps/stationmap_metpy.py" $path_input $path_output_stationt
+python /localdata/weathermaps/imuk/products/stationmaps/stationmap_metpy.py $path_input $path_output_stationt
